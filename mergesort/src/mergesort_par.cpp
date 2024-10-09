a4999722cf50b83d4857661326ae3d8a52d1d86d
Vladislav Kuznetsov <starforce882@gmail.com>
2024-10-09 13:42:29 +0200

    OpenMP test successful

--- mergesort/src/mergesort_par.cpp
+++ mergesort/src/mergesort_par.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <cstdlib>
+#include <ctime>
+#include "omp.h"
 
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World! -- parallel");
-   return 0;
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        printf("Hello from thread %i!\n", thread_id);
+    }
+
+
+    return 0;
 }
