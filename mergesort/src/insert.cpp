1f7dbc3959c7722477550c4fe016e319772b3330
Vladislav Kuznetsov <starforce882@gmail.com>
2024-10-23 14:44:41 +0200

    Implement compairing

--- mergesort/src/insert.cpp
+++ mergesort/src/insert.cpp
@@ -6,21 +6,24 @@
 #include <sstream>
 #include <iomanip>
 #include <random>
+#include <fstream>
 
 // struct for some algorithm analysis statistics
 struct Stats {
     u_long comparisons;    // comparisons number
     u_long acceses;        // array accesses number
+    double a;              // current amount comparisons to the total amount of pairs to compare
 
-    Stats() : comparisons(0), acceses(0) {} // initialize stats
+    Stats() : comparisons(0), acceses(0), a(0.0) {} // initialize stats
 };
 
 void populate(float* arr, int size) {
-    std::mt19937 gen(std::random_device{}());
+    //std::random_device{}()
+    std::mt19937 gen(69);
     std::uniform_real_distribution<> dis(0.0, 255.0);
     for (int i = 0; i < size; i++) {
         arr[i] = dis(gen);
-}
+    }
 }
 
 Stats sort(float* arr, int size) {
@@ -31,13 +34,13 @@ Stats sort(float* arr, int size) {
         float current = arr[i]; // cache the current element to be inserted
         stats.acceses++;        // track array access
         int j = i - 1;          // counter for compairing the left part of the array
-       
+
         // compare elements to the left
         // while there are elements to the left AND
         // while the element to the left is bigger than the current element
         // e.g. 8 and 4
         while (j >= 0 && arr[j] > current) {
-            stats.comparisons += 2; // two comparisons above, not one
+            stats.comparisons++; // two comparisons above, not one
             stats.acceses++;
             // temporarily copy the element to the right to the left
             // e.g. 8 and 8 
@@ -51,12 +54,15 @@ Stats sort(float* arr, int size) {
         arr[j+1] = current;
         stats.acceses++;
     }
+    // comparisons divided by the total pairs binomial coefficient
+    stats.a = static_cast<double>(stats.comparisons) / ((size * (size - 1) / 2)); 
+
     return stats;
 }
 
 std::string formatNumber(uint64_t num) {
     std::stringstream ss;
-    ss.imbue(std::locale(""));   // regional thing. en_US.UTF-8 for commas
+    ss.imbue(std::locale("en_US.UTF-8"));   // regional thing. en_US.UTF-8 for commas
     ss << num;
     return ss.str();
 }
@@ -79,7 +85,7 @@ std::string formatDuration(uint64_t durationUs) {
     return oss.str();
 }
 
-void runSimulation(float* arr, int size) {
+Stats runSimulation(float* arr, int size) {
     populate(arr, size);
 
     auto start = std::chrono::high_resolution_clock::now();
@@ -88,24 +94,43 @@ void runSimulation(float* arr, int size) {
 
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
 
-    printf("Simulation of size %s done in %s with %s comparisons.\n",
-            formatNumber(size).c_str(),
-            formatDuration(duration).c_str(),
-            formatNumber(stats.comparisons).c_str());
+    // printf("Simulation of size %s done in %s with %s comparisons.\n",
+    //         formatNumber(size).c_str(),
+    //         formatDuration(duration).c_str(),
+    //         formatNumber(stats.comparisons).c_str());
+
+    return stats;
 }
 
 int main() {
-    std::srand(std::time(0));
+    //std::srand(std::time(0));
 
-    int startSize = 1;
-    int endSize = 500;
+    int startSize = 3;
+    int endSize = 50;
+    int simulations = 50;
     float* arr = new float[endSize]{ };
 
     printf("Simulating single-threaded sorting numbers using insertion sort, please wait...\n");
+
+    std::ofstream outFile("../simulation_results.csv");
+
+    outFile << "Size,Simulations,Average a\n";
+
     for(int size = startSize; size <= endSize; size++) {
-        runSimulation(arr, size);
+        double totalA = 0.0;
+
+        // run simulation for 50 times for each array with current size and get
+        // the average a
+        for(int i = 0; i <= simulations; i++) {
+            Stats stats = runSimulation(arr, size);
+            totalA += stats.a;
+        }
+        double averageA = totalA / simulations;
+        printf("Average 'a' for size %d after %d simulations: %.5f\n", size, simulations, averageA);
+        outFile << size << "," << simulations << "," << averageA << "\n";
     }
 
+    outFile.close();
     delete[] arr;
     return 0;
 }
