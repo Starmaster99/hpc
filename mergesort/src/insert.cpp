17a3407edcf26132c6281a318421a531190ceb3b
Vladislav Kuznetsov <starforce882@gmail.com>
2024-11-17 16:23:21 +0100

    Mergesort complete

--- mergesort/src/insert.cpp
+++ mergesort/src/insert.cpp
@@ -10,16 +10,15 @@
 
 // struct for some algorithm analysis statistics
 struct Stats {
-    u_long comparisons;    // comparisons number
-    u_long acceses;        // array accesses number
+    unsigned comparisons;    // comparisons number
+    unsigned acceses;        // array accesses number
     double a;              // current amount comparisons to the total amount of pairs to compare
 
     Stats() : comparisons(0), acceses(0), a(0.0) {} // initialize stats
 };
 
-void populate(float* arr, int size) {
-    //std::random_device{}()
-    std::mt19937 gen(69);
+void populate(float* arr, int size, int seed) {
+    std::mt19937 gen(seed);
     std::uniform_real_distribution<> dis(0.0, 255.0);
     for (int i = 0; i < size; i++) {
         arr[i] = dis(gen);
@@ -27,34 +26,23 @@ void populate(float* arr, int size) {
 }
 
 Stats sort(float* arr, int size) {
-    Stats stats;    // an instance to track statistics  
-    // start from the second element since the first one has already been sorted
-    // and iterate over until we reach the end of the   array
-    for (int i = 1; i < size; i++) {
-        float current = arr[i]; // cache the current element to be inserted
-        stats.acceses++;        // track array access
-        int j = i - 1;          // counter for compairing the left part of the array
+    Stats stats;
 
-        // compare elements to the left
-        // while there are elements to the left AND
-        // while the element to the left is bigger than the current element
-        // e.g. 8 and 4
+    for (int i = 1; i < size; i++) {
+        float current = arr[i];
+        stats.acceses++;
+        int j = i - 1; 
+
         while (j >= 0 && arr[j] > current) {
-            stats.comparisons++; // two comparisons above, not one
+            stats.comparisons++;
             stats.acceses++;
-            // temporarily copy the element to the right to the left
-            // e.g. 8 and 8 
             arr[j+1] = arr[j];
             stats.acceses++;
-            // move the pointer backwards
             j--;
         }
-        // and copy the cached element (4) to the left
-        // e.g. 4 and 8
         arr[j+1] = current;
         stats.acceses++;
     }
-    // comparisons divided by the total pairs binomial coefficient
     stats.a = static_cast<double>(stats.comparisons) / ((size * (size - 1) / 2)); 
 
     return stats;
@@ -85,19 +73,14 @@ std::string formatDuration(uint64_t durationUs) {
     return oss.str();
 }
 
-Stats runSimulation(float* arr, int size) {
-    populate(arr, size);
+Stats runSimulation(float* arr, int size, int seed) {
+    populate(arr, size, seed);
 
     auto start = std::chrono::high_resolution_clock::now();
     Stats stats = sort(arr, size);
     auto end = std::chrono::high_resolution_clock::now();
 
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-
-    // printf("Simulation of size %s done in %s with %s comparisons.\n",
-    //         formatNumber(size).c_str(),
-    //         formatDuration(duration).c_str(),
-    //         formatNumber(stats.comparisons).c_str());
 
     return stats;
 }
@@ -106,11 +89,12 @@ int main() {
     int startSize = 3;
     int endSize = 50;
     int simulations = 50;
+    int seed = std::random_device{}();
     float* arr = new float[endSize]{ };
 
     printf("Simulating single-threaded sorting numbers using insertion sort, please wait...\n");
 
-    std::ofstream outFile("../simulation_results.csv");
+    std::ofstream outFile("../simulation_results_insert.csv");
 
     outFile << "Size,Simulations,Average a\n";
 
@@ -120,7 +104,7 @@ int main() {
         // run simulation for 50 times for each array with current size and get
         // the average a
         for(int i = 0; i <= simulations; i++) {
-            Stats stats = runSimulation(arr, size);
+            Stats stats = runSimulation(arr, size, seed);
             totalA += stats.a;
         }
         double averageA = totalA / simulations;
---
+++ mergesort/src/mergesort.cpp
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <chrono>
+#include <fstream>
+
+unsigned comparisons = 0;
+double a = 0.0f;
+
+void populate(float* arr, int size, int seed) {
+    std::mt19937 gen(seed);
+    std::uniform_real_distribution<> dis(0.0, 255.0);
+    for (int i = 0; i < size; i++) {
+        arr[i] = dis(gen);
+    }
+}
+
+void merge(float* left, float* right, float* arr, int size) {
+    int leftSize = size / 2;
+    int rightSize = size - leftSize;
+
+    int i = 0, l = 0, r = 0;
+
+    while (l < leftSize && r < rightSize) {
+        comparisons++; // Increment comparison counter
+        if (left[l] < right[r]) {
+            arr[i++] = left[l++];
+        } else {
+            arr[i++] = right[r++];
+        }
+    }
+
+    while (l < leftSize) {
+        arr[i++] = left[l++];
+    }
+    while (r < rightSize) {
+        arr[i++] = right[r++];
+    }
+}
+
+void sort(float* arr, int size) {
+    if (size <= 1) return;
+    int middle = size / 2;
+
+    float* left  = new float[middle]{ };
+    float* right = new float[size - middle]{ };
+
+    for (int i = 0; i < middle; i++) {
+        left[i] = arr[i];
+    }
+    for (int i = 0; i < size - middle; i++) {
+        right[i] = arr[middle + i];
+    }
+
+    sort(left, middle);
+    sort(right, size - middle);
+
+    merge(left, right, arr, size);
+
+    delete[] right;
+    delete[] left;
+}
+
+std::string formatNumber(uint64_t num) {
+    std::string str = std::to_string(num);
+
+    int insertPos = str.length() - 3;
+    while(insertPos > 0) {
+        str.insert(insertPos, ",");
+        insertPos -= 3;
+    }
+
+    return str;
+}
+
+void print(float* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%f ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+std::string formatDuration(uint64_t durationUs) {
+    if (durationUs < 10) {
+        return "<0 µs";
+    }
+    else if(durationUs < 1000) {
+        // microseconds
+        return std::to_string(static_cast<double>(durationUs)) + "µs"; 
+    }
+    else if(durationUs < 1000000) { 
+        // milliseconds
+        return std::to_string(static_cast<double>(durationUs) / 1000) + "ms";
+    }
+    else {
+        // seconds
+        return std::to_string(static_cast<double>(durationUs) / 1000000) + "s";
+    } 
+}
+
+void runSimulation(float* arr, int iterations, int arraySize, int seed) {
+    double totalComparisons = 0.0;
+    double totalA = 0.0;
+
+    for (int i = 0; i < iterations; i++) {
+
+        populate(arr, arraySize, seed);
+
+        comparisons = 0;
+        sort(arr, arraySize);
+
+        a = static_cast<double>(comparisons) / ((arraySize * (arraySize - 1)) / 2);
+
+        totalComparisons += comparisons;
+        totalA += a;
+    }
+
+    double averageComparisons = totalComparisons / iterations;
+    double averageA = totalA / iterations;
+
+    printf("Size: %i\nAverage comparisons: %f\nAverage 'a': %f\n", 
+            arraySize, averageComparisons, averageA);
+}
+
+int main() {
+    int startSize = 3;
+    int endSize = 50;
+    int iterations = 5000;
+    int seed = std::random_device{}();
+    float* arr = new float[endSize]{ };
+
+    std::ofstream outFile("../simulation_results_merge.csv");
+    outFile << "Size,Simulations,Average a\n";
+
+    for(int size = startSize; size <= endSize; size++) {
+        runSimulation(arr, iterations, size, seed);
+    }
+
+    delete[] arr;
+    return 0;
+}
